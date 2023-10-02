ed78461836291d30f693020119da87418f736103
adi2730 <chahar.aditya12@gmail.com>
2023-10-02 17:27:31 +0530

    password validation fix

--- src/app/user-login/user-login.component.ts
+++ src/app/user-login/user-login.component.ts
@@ -28,7 +28,7 @@ export class UserLoginComponent implements OnInit {
       email: ['', [Validators.required,  Validators.pattern(
         /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/
       )]],
-      password: ['', Validators.required],
+      password: ['', [Validators.required,Validators.minLength(6)]],
     });
   }
 
